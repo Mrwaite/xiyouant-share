c5228abc4331d771ed20cb94835e69bcbdd81ab8
Mrwaite <mrwaite1996@gmail.com>
2016-12-04 18:05:46 +0800

    begin

---
+++ src/main.js
@@ -0,0 +1,30 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
+
+import App from './App'
+import Home from './components/Home'
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+const routes = [{
+  path : '/',
+  component : Home
+},{
+  path : '/home',
+  component : Home
+}];
+
+const router = new VueRouter({
+  routes
+});
+
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  ...App,
+})
